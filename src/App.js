78f93fccbcde73c506f8dd53fd71515223b1fc58
Ekaterina Krapivina <binaryrnd@gmail.com>
2022-04-25 17:00:09 +0400

    Feature branch initialised

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Beeracle</h1>
+        <h1>Beeracle</h1>
+        <h4>A new generation app for exploring and assessing beer.</h4>
     </div>
   );
 }
